39559f9aa75d1c5d74dcbd8515e69980c58e3ea9
Adelekan Oba <aoba@jaguarlandrover.com>
2023-01-29 20:34:24 +0000

    Main User Interface Implementation Completed.

--- SLDCProject/app.py
+++ SLDCProject/app.py
@@ -4,6 +4,10 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask
+from flask import render_template
+
+
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -12,8 +16,25 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/statistic')
+def statistic():
+    return render_template('statistic.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8080, debug=True)
 
 if __name__ == '__main__':
     import os
