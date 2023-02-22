026ebf3fef6505bf2947eafa620370db6af18176
Adelekan Oba <aoba@jaguarlandrover.com>
2023-02-22 18:22:34 +0000

    Completion of test cases using pytest.

--- SLDCProject/test_app.py
+++ SLDCProject/test_app.py
@@ -6,6 +6,8 @@ import os
 from flask import session
 
 
+
+
 @pytest.fixture
 def client(request):
     app.app.config['TESTING'] = True
@@ -23,16 +25,31 @@ def test_addData(client):
 
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
-    response = client.post('/addData2022', data={'module': 'SDLC Tutor', 
+
+
+    response = client.post('/addData2019', data={'module': 'Test Data', 
                                                     'grade': '60', 
                                                     'percentage': '80'})
 
-    c.execute("SELECT * FROM statistic2022 WHERE moduleName='SDLC Tutor'")
-    insertData = c.fetchall()
+    assert response.status_code == 302
     conn.commit()
-    assert insertData[0][0] == "SDLC Tutor"
+    c.execute("SELECT * FROM statistic2019 WHERE moduleName='Test Data'")
+    insertData = c.fetchall()
     assert len(insertData) > 0
+    assert insertData[0][0] == "Test Data"
+
     conn.close()
+
+    
+def test_addDataInvalidPrivileges(client):
+    with client.session_transaction() as session:
+        session['user'] = "student@warwick.ac.uk"
+
+    response = client.post('/addData2019', data={'module': 'module', 
+                                                    'grade': '60', 
+                                                    'percentage': '70'})
+
+    assert response.status_code == 403
 
 
 
@@ -43,28 +60,50 @@ def test_updateData(client):
 
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
+    c.execute("DELETE FROM statistic2019")
+
     c.execute("INSERT INTO statistic2019 (moduleName, grade, percentage) VALUES (?,?,?)", 
               ('Agile Project Management', 40, 30))
     conn.commit()
-    conn.close()
 
-    # Update the sample data
     response = client.post('/updateData2019', data={'module': 'Agile Project Management', 
                                                     'grade': '50', 
                                                     'percentage': '90'})
     assert response.status_code == 302
 
-    # Verify the data was updated
-    conn = sqlite3.connect('wmgsisDB.db')
-    c = conn.cursor()
+
+
     c.execute("SELECT * FROM statistic2019 WHERE moduleName='Agile Project Management'")
     updatedData = c.fetchall()
-    conn.commit()
     assert len(updatedData) > 0
     assert updatedData[0][0] == "Agile Project Management"
     assert updatedData[0][1] == 50
     assert updatedData[0][2] == 90
+
+    c.execute("DELETE FROM statistic2019 WHERE moduleName='Agile Project Management'")
+    conn.commit()
     conn.close()
+
+def test_updateDataInvalidPrivileges(client):
+    with client.session_transaction() as session:
+        session['user'] = "student@warwick.ac.uk"
+
+    response = client.post('/updateData2019', data={'module': 'Agile Project Management', 
+                                                    'grade': '60', 
+                                                    'percentage': '70'})
+
+    assert response.status_code == 403
+
+
+def test_deleteModuleInvalidPrivileges(client):
+    with client.session_transaction() as session:
+        session['user'] = "student@warwick.ac.uk"
+
+    response = client.post('/deleteModule2019', data={'module': 'Agile Project Management', 
+                                                    'grade': '60', 
+                                                    'percentage': '70'})
+
+    assert response.status_code == 403
 
 
 
@@ -82,13 +121,13 @@ def test_deleteModule(client):
     conn.commit()
     conn.close()
 
-    # Delete the sample data
+  
     response = client.post('/deleteModule2022', data={'module': 'Internet of Things',
-                                                     'grade': '60', 
-                                                     'percentage': '80'})
+                                                     'grade': '90', 
+                                                     'percentage': '75'})
     assert response.status_code == 302
 
-    # Verify the data was deleted
+   
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
     c.execute("SELECT * FROM statistic2022 WHERE moduleName='Internet of Things'")
@@ -103,9 +142,20 @@ def test_deleteData(client):
 
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
-    c.execute("DELETE FROM statistic2019")
+
+    response = client.post('/deleteData2019')
+
+    assert response.status_code == 302
+
     c.execute("SELECT * FROM statistic2019")
     clearedData = c.fetchall()
     assert len(clearedData) == 0
-    conn.commit()
     conn.close()
+
+def test_deleteDataInvalidPrivileges(client):
+    with client.session_transaction() as session:
+        session['user'] = "student@warwick.ac.uk"
+
+    response = client.post('/deleteData2019')
+
+    assert response.status_code == 403
