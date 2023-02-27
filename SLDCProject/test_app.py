a024772155a1d6ae35eb3f18bd3002892fba9b78
Adelekan Oba <aoba@jaguarlandrover.com>
2023-02-27 23:49:26 +0000

    Pytests completed and commented.

--- SLDCProject/test_app.py
+++ SLDCProject/test_app.py
@@ -3,8 +3,7 @@ import sqlite3
 import pytest
 import unittest
 import os 
-from flask import session
-
+from flask import session, url_for
 
 
 
@@ -20,21 +19,27 @@ def client(request):
     return client
 
 def test_addData(client):
+
+    #Open new session with the test client 
     with client.session_transaction() as session:
+        #Set the session as a tutor so they have the appropriate privileges
         session['user'] = "tutor@warwick.ac.uk"
 
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
 
-
+    #Add test data 
     response = client.post('/addData2019', data={'module': 'Test Data', 
                                                     'grade': '60', 
                                                     'percentage': '80'})
 
+    #Ensure the user has been redirected, meaning the insert was successful
     assert response.status_code == 302
     conn.commit()
+    #Ensure the data has been added into the database 
     c.execute("SELECT * FROM statistic2019 WHERE moduleName='Test Data'")
     insertData = c.fetchall()
+    #Ensure there is an entry for Test Data
     assert len(insertData) > 0
     assert insertData[0][0] == "Test Data"
 
@@ -49,6 +54,7 @@ def test_addDataInvalidPrivileges(client):
                                                     'grade': '60', 
                                                     'percentage': '70'})
 
+    #Ensure user is forbidden from carrying out CRUD function
     assert response.status_code == 403
 
 
@@ -61,18 +67,20 @@ def test_updateData(client):
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
     c.execute("DELETE FROM statistic2019")
-
+    
+    #Insert Test Data to be updated 
     c.execute("INSERT INTO statistic2019 (moduleName, grade, percentage) VALUES (?,?,?)", 
               ('Agile Project Management', 40, 30))
     conn.commit()
 
+    #Update the test data 
     response = client.post('/updateData2019', data={'module': 'Agile Project Management', 
                                                     'grade': '50', 
                                                     'percentage': '90'})
     assert response.status_code == 302
 
 
-
+    #Ensure the test data was updated correctly
     c.execute("SELECT * FROM statistic2019 WHERE moduleName='Agile Project Management'")
     updatedData = c.fetchall()
     assert len(updatedData) > 0
@@ -92,6 +100,7 @@ def test_updateDataInvalidPrivileges(client):
                                                     'grade': '60', 
                                                     'percentage': '70'})
 
+    #Ensure user is forbidden from carrying out CRUD function
     assert response.status_code == 403
 
 
@@ -103,6 +112,7 @@ def test_deleteModuleInvalidPrivileges(client):
                                                     'grade': '60', 
                                                     'percentage': '70'})
 
+    #Ensure user is forbidden from carrying out CRUD function
     assert response.status_code == 403
 
 
@@ -113,15 +123,16 @@ def test_deleteModule(client):
 
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
+    #Insert test data to be deleted
     c.execute("INSERT INTO statistic2022 (moduleName, grade, percentage) VALUES (?,?,?)", 
               ('Internet of Things', 90, 75))
     c.execute("SELECT * FROM statistic2022 WHERE moduleName='Internet of Things'")
-    insertData = c.fetchall()
-    assert len(insertData) > 0
+    insertedData = c.fetchall()
+    assert len(insertedData) > 0
     conn.commit()
     conn.close()
 
-  
+    #Delete the test data
     response = client.post('/deleteModule2022', data={'module': 'Internet of Things',
                                                      'grade': '90', 
                                                      'percentage': '75'})
@@ -130,6 +141,7 @@ def test_deleteModule(client):
    
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
+    #Ensure the test data was deleted
     c.execute("SELECT * FROM statistic2022 WHERE moduleName='Internet of Things'")
     deletedData = c.fetchall()
     assert len(deletedData) == 0
@@ -143,12 +155,15 @@ def test_deleteData(client):
     conn = sqlite3.connect('wmgsisDB.db')
     c = conn.cursor()
 
+    #Clear the data
     response = client.post('/deleteData2019')
 
     assert response.status_code == 302
 
     c.execute("SELECT * FROM statistic2019")
     clearedData = c.fetchall()
+
+    #Ensure the data has been deleted from the table
     assert len(clearedData) == 0
     conn.close()
 
@@ -158,4 +173,55 @@ def test_deleteDataInvalidPrivileges(client):
 
     response = client.post('/deleteData2019')
 
+    #Ensure user is forbidden from carrying out CRUD function
     assert response.status_code == 403
+
+
+
+def test_signIn(client):
+    with client:
+        response = client.post('/signIn', data={'email': 'tutor@warwick.ac.uk', 'password': '1111'})
+        #Login using correct credentials
+        assert response.status_code == 200
+        assert 'user' in session
+        assert session['user'] == 'tutor@warwick.ac.uk'
+
+def test_signInInvalidCredentials(client):
+    with client:
+        response = client.post('/signIn', data={'email': 'tutor@warwick.ac.uk', 'password': 'incorrectPassword'})
+        #Login using invalid credentials
+        assert b'Error: Incorrect E-Mail and/or password' in response.data
+        assert 'user' not in session
+
+def test_InPersonClassesPage(client):
+    #Set the session as a tutor
+    with client.session_transaction() as session:
+        session['user'] = "tutor@warwick.ac.uk"
+
+    response = client.get('/inPersonClasses2019')
+    #Page loads up OK
+    assert response.status_code == 200
+    
+
+    with client.session_transaction() as session:
+        #Set the session as a student
+        session['user'] = "student@warwick.ac.uk"
+
+    response = client.get('/inPersonClasses2019')
+    #Page loads up OK
+    assert response.status_code == 200
+    
+    with client.session_transaction() as session:
+        #Set invalid credentials
+        session['user'] = "invalidCredentials@warwick.ac.uk"
+
+    response = client.get('/inPersonClasses2019')
+    #User is redirected to the login page
+    assert response.status_code == 302
+    assert response.location.endswith('/login')
+
+    
+
+        
+
+
